e6f31e9139284395c04fd4d6ce5e40922c584bdd
thiunuwan <ravinduthiunuwan@gmail.com>
2023-06-24 05:18:03 +0530

    add resume & update styles

--- src/script.js
+++ src/script.js
@@ -1,15 +1,32 @@
-const text = "Thiunuwan";
-const span = document.getElementById("typewriter");
-let index = 0;
+// const text = "Thiunuwan";
+// const span = document.getElementById("typewriter");
+// let index = 0;
 
-function type() {
-  span.textContent += text.charAt(index);
-  index++;
-  if (index >= text.length) {
-    index = 0;
-    span.textContent = "";
-  }
-  setTimeout(type, 500);
+// function type() {
+//   span.textContent += text.charAt(index);
+//   index++;
+//   if (index >= text.length) {
+//     index = 0;
+//     span.textContent = "";
+//   }
+//   setTimeout(type, 500);
+// }
+
+// type();
+
+var msgArray= ["Thiunuwan"];
+var textPosition =-1;
+var speed =125;
+
+typewriter=() =>{
+    document.querySelector("#message").
+    innerHTML=msgArray[0].substring(0,textPosition)+"<span></span>";
+
+    if(textPosition++ != msgArray[0].length)
+    setTimeout(typewriter,speed);
 }
 
-type();
+
+window.addEventListener("load",typewriter);
+
+
